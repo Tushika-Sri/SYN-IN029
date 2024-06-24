98e4c231245411b7a584aa6ab662a9ab88169c5c
Tushika Srivastava <tushika@BHAVYAs-MacBook-Air.local>
2024-06-24 12:32:24 +0530

    Adding comments

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,11 +4,14 @@ class PrimeNumber{
 	{
 		if (n <= 1)
 			return false;
-        
+        //Loop from 2 to the square root of the number
         for (int i = 2; i <= Math.sqrt(n); i++)
+		{
+			//If number is divisible by any i, it is not a prime
 			if (n % i == 0)
 				return false;
-        
+		}
+		//If no divisors are found, number is prime
         return true;
 	}
     public static void main(String args[])
